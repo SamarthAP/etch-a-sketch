43727cc60522f80fefb211addce1aeac56954a6e
Samarth Patel <samarth.patel.22@hotmail.com>
2018-11-08 00:01:19 -0500

    Created a working grid

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ var doc = document.querySelector('.container');
 
 for (let j = 0; j < 256; j++) {
     let div = document.createElement('div');
-    div.textContent = j;
-    div.classList.add('item');
+    //div.textContent = j;
+    div.classList.add('item1');
+
+    div.addEventListener('mouseover', (e) => {
+        div.classList.remove('item1');
+        div.classList.add('item2');
+    })
 
     doc.appendChild(div);
 }
