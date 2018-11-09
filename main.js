c5dca3d508ad826909872aa14fdbe18c3ed16a64
Samarth Patel <samarth.patel.22@hotmail.com>
2018-11-08 19:19:24 -0500

    Added functionality to reset grid and change number of squares

--- main.js
+++ main.js
@@ -1,14 +1,54 @@
-var doc = document.querySelector('.container');
+var container = document.querySelector('.container');
 
-for (let j = 0; j < 256; j++) {
-    let div = document.createElement('div');
-    //div.textContent = j;
-    div.classList.add('item1');
+var doc = document.querySelector('body');
 
-    div.addEventListener('mouseover', (e) => {
-        div.classList.remove('item1');
-        div.classList.add('item2');
-    })
+let button = document.createElement('button');
+button.textContent = 'Reset';
 
-    doc.appendChild(div);
+doc.appendChild(button);
+
+button.addEventListener('click', (e) => {
+    for (let i = 0; i < container.children.length; i++) {
+        container.children[i].classList.add('item1');
+        container.children[i].classList.remove('item2');
+
+        
+    }
+
+    let n = prompt("How many squares per side?", "16");
+    parseInt(n);
+
+    removeOldGrid();
+
+    createGrid(n);
+})
+
+createGrid(16);
+
+function createGrid(n) {
+    let cont = document.getElementsByClassName('container');
+    cont[0].style.gridTemplateColumns = 'auto '.repeat(n);
+
+    for (let j = 0; j < n*n; j++) {
+        let div = document.createElement('div');
+    
+        div.classList.add('item1');
+
+        let scaled = (30/(n/16)) + 'pt';
+        div.style.width = scaled;
+        div.style.height = scaled;
+    
+        div.addEventListener('mouseover', (e) => {
+            div.classList.remove('item1');
+            div.classList.add('item2');
+        })
+    
+        container.appendChild(div);
+    }
+}
+
+function removeOldGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    } 
 }
